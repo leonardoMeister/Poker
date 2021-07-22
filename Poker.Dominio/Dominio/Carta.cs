bd4e29f828e78db0de335059f744c2073f93b9bb
leonardo Meister <leozinhomm1@gmail.com>
2021-07-22 13:58:27 -0300

    Adicionar arquivos de projeto.

---
+++ Poker.Dominio/Dominio/Carta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker.Dominio.Dominio
+{
+    public class Carta
+    {
+        public string Naipe { get; set; }
+
+        public string Valor { get; set; }
+
+    }
+}
---
+++ Poker.Teste/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Poker.Teste")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Poker.Teste")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f39cff53-cd28-414e-a9f7-f9fb057e832d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
